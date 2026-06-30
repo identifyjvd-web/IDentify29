1c807c1c9f3a65a3257182324d8883dabdca2ee4
identifyjvd-web <identify.jvd@gmail.com>
2026-06-30 08:07:54 +0530

    Delete temp_script_15.js

--- temp_script_15.js
+++
@@ -1,9 +0,0 @@
-
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', () => {
-                navigator.serviceWorker.register('./sw.js').catch(err => {
-                    console.log('Service Worker registration failed:', err);
-                });
-            });
-        }
-    
